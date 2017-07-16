ef0ffc9e371c307a07ec035387e97387b7724e09
Dr Bean <drbean@freeshell.org>
2017-07-16 16:33:14 +0800

    from_truefalse.with_group

--- lang/en/qtype_truefalsegroup.php
+++ lang/en/qtype_truefalsegroup.php
@@ -25,4 +25,20 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$string['pluginname'] = 'truefalsegroup';
+// $string['pluginname'] = 'truefalsegroup';
+
+$string['correctanswer'] = 'Correct answer';
+$string['correctanswerfalse'] = 'The correct answer is \'False\'.';
+$string['correctanswertrue'] = 'The correct answer is \'True\'.';
+$string['false'] = 'False';
+$string['feedbackfalse'] = 'Feedback for the response \'False\'.';
+$string['feedbacktrue'] = 'Feedback for the response \'True\'.';
+$string['pleaseselectananswer'] = 'Please select an answer.';
+$string['selectone'] = 'Select one:';
+$string['true'] = 'True';
+$string['pluginname'] = 'True/False with group grade';
+$string['pluginname_help'] = 'In response to a question (that may include a image) the respondent chooses from true or false, and gets group grade.';
+$string['pluginname_link'] = 'question/type/truefalsegroup';
+$string['pluginnameadding'] = 'Adding a True/False question';
+$string['pluginnameediting'] = 'Editing a True/False question';
+$string['pluginnamesummary'] = 'A simple form of multiple choice question with just the two choices \'True\' and \'False\' and a group grade.';
