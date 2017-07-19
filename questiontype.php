34778ea726c2182dd09fdb829c2a2703df281812
Dr Bean <drbean@freeshell.org>
2017-07-19 19:46:39 +0800

    s/questionlib.php/truefalse.questiontype.php/.to_find_qtype_truefalse_class

--- questiontype.php
+++ questiontype.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-require_once($CFG->libdir.'/questionlib.php');
+require_once($CFG->dirroot . '/question/type/truefalse/questiontype.php');
 
 /**
  * Class that represents a truefalsegroup question type.
