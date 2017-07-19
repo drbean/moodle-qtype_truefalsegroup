b02e95f16c3fd2af6a295eda3e04bc54b0f069e6
Dr Bean <drbean@freeshell.org>
2017-07-19 19:48:14 +0800

    s/20171601/2017071900/

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_truefalsegroup';
 $plugin->release = '0.1.0';
-$plugin->version = 2017071601;
+$plugin->version = 2017071900;
 $plugin->requires = 2017051500;
 $plugin->maturity = MATURITY_ALPHA;
